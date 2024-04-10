3b5aba7d539d867d46498e39c7d523dc87518396
LuizMafraJNR <luiz.sesna@univille.br>
2024-04-09 21:26:31 -0300

    Removendo metodos não utilizados e validação de genero valido.

--- src/main/java/univille/activity/Main.java
+++ src/main/java/univille/activity/Main.java
@@ -1,12 +1,9 @@
 package univille.activity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-
 import univille.activity.domain.model.Student;
 import univille.activity.stack.Stack;
+
+import java.util.Scanner;
 
 public class Main
 {
@@ -16,9 +13,6 @@ public class Main
 		Stack pe1 = new Stack(20);
 		Stack pe2 = new Stack(20);
 
-		// TODO - Aqui os estudantes deve ser inseridos por Scanner.
-		//  Fazer um IF para se caso for M adicionar na pe1 e F na p2
-
 		String stop = "n";
 		do {
 			System.out.println("Enter the student name: ");
@@ -27,6 +21,11 @@ public class Main
 			int registration = scanner.nextInt();
 			System.out.println("Enter the student gender: ");
 			char gender = String.valueOf(scanner.next().charAt(0)).toUpperCase().charAt(0);
+			if (gender != 'M' || gender != 'F' || gender != 'm' || gender != 'f')
+			{
+				System.out.println("Enter a valid gender (M/F)");
+				gender = String.valueOf(scanner.next().charAt(0)).toUpperCase().charAt(0);
+			}
 			System.out.println("Do you want stop? (s/n)");
 			stop = scanner.next();
 
@@ -54,64 +53,4 @@ public class Main
 					student.getRegistration(), student.getGender()));
 		}
 	}
-
-	// TODO - REMOVE
-	static List<Student> getStudentsM()
-	{
-		List<Student> students = new ArrayList<>();
-
-		students.add(new Student("John1", 1001, 'M'));
-		students.add(new Student("John2", 1002, 'M'));
-		students.add(new Student("John3", 1003, 'M'));
-		students.add(new Student("John4", 1004, 'M'));
-		students.add(new Student("John5", 1005, 'M'));
-		students.add(new Student("John6", 1006, 'M'));
-		students.add(new Student("John7", 1007, 'M'));
-		students.add(new Student("John8", 1008, 'M'));
-		students.add(new Student("John9", 1009, 'M'));
-		students.add(new Student("John10", 1010, 'M'));
-		students.add(new Student("John11", 1011, 'M'));
-		students.add(new Student("John12", 1012, 'M'));
-		students.add(new Student("John13", 1013, 'M'));
-		students.add(new Student("John14", 1014, 'M'));
-		students.add(new Student("John15", 1015, 'M'));
-		students.add(new Student("John16", 1016, 'M'));
-		students.add(new Student("John17", 1017, 'M'));
-		students.add(new Student("John18", 1018, 'M'));
-		students.add(new Student("John19", 1019, 'M'));
-		students.add(new Student("John20", 1020, 'M'));
-
-		return students;
-	}
-
-
-	// TODO - REMOVE
-	static List<Student> getStudentsF()
-	{
-		List<Student> students = new ArrayList<>();
-
-		students.add(new Student("Jane1", 2001, 'F'));
-		students.add(new Student("Jane2", 2002, 'F'));
-		students.add(new Student("Jane3", 2003, 'F'));
-		students.add(new Student("Jane4", 2004, 'F'));
-		students.add(new Student("Jane5", 2005, 'F'));
-		students.add(new Student("Jane6", 2006, 'F'));
-		students.add(new Student("Jane7", 2007, 'F'));
-		students.add(new Student("Jane8", 2008, 'F'));
-		students.add(new Student("Jane9", 2009, 'F'));
-		students.add(new Student("Jane10", 2010, 'F'));
-		students.add(new Student("Jane11", 2011, 'F'));
-		students.add(new Student("Jane12", 2012, 'F'));
-		students.add(new Student("Jane13", 2013, 'F'));
-		students.add(new Student("Jane14", 2014, 'F'));
-		students.add(new Student("Jane15", 2015, 'F'));
-		students.add(new Student("Jane16", 2016, 'F'));
-		students.add(new Student("Jane17", 2017, 'F'));
-		students.add(new Student("Jane18", 2018, 'F'));
-		students.add(new Student("Jane19", 2019, 'F'));
-		students.add(new Student("Jane20", 2020, 'F'));
-
-		return students;
-	}
-
 }
